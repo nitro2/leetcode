a80d0cb0ae7b63ad3d88ba0acaad3edb0ac318e6
Nitro <ngothanhnhannt@gmail.com>
2022-07-20 15:33:41 +0700

    consecutive-numbers-sum: Done 100% faster

--- consecutive-numbers-sum/consecutive.go
+++ consecutive-numbers-sum/consecutive.go
@@ -1,6 +1,6 @@
+// Runtime: 4 ms, faster than 100.00% of Go online submissions for Consecutive Numbers Sum.
+// Memory Usage: 2 MB, less than 71.43% of Go online submissions for Consecutive Numbers Sum.
 package consecutive
-
-import "fmt"
 
 func consecutiveNumbersSum(n int) int {
 	if n <= 2 {
@@ -9,11 +9,11 @@ func consecutiveNumbersSum(n int) int {
 	// x = n/r - (r-1)/2  --> Find integer of x
 	count := 1
 	x := 0.0
-	for r := 2.0; r < float64(n) && x >= 0; r++ {
+	for r := 2.0; r*r < float64(2*n) && x >= 0; r++ {
 		x = (float64(n)/r - float64(r-1)/2)
 		// fmt.Println(n, x, r)
 		if x > 0 && x == float64(int(x)) {
-			fmt.Println(n, x, r)
+			// fmt.Println(n, x, r)
 			count++
 		}
 	}
